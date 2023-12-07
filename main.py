5fc9e2f7c5cec5218f8e92be9bb6f329ebb54679
Ricardo Chan <ricardochan380@gmail.com>
2023-12-07 08:26:10 -0500

    Initial commit
    The ISS Location App provides real-time data on the International Space Station's (ISS) current position.

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+def get_iss_location():
+    # API endpoint for ISS current location
+    api_url = "https://api.wheretheiss.at/v1/satellites/25544"
+
+    try:
+        # Send GET request to the API
+        response = requests.get(api_url)
+        response.raise_for_status()  # Raise an exception for bad responses
+
+        # Parse the JSON data
+        iss_data = response.json()
+
+        # Extract relevant information
+        latitude = iss_data["latitude"]
+        longitude = iss_data["longitude"]
+
+        print(f"ISS Current Location: Latitude {latitude}, Longitude {longitude}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching ISS location: {e}")
+
+if __name__ == "__main__":
+    get_iss_location()
